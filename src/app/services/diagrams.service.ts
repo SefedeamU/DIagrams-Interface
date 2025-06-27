7fdbf79009424c6661e6d71f7791aa635249581f
Sefedeam <sergio.delgado.a@utec.edu.pe>
2025-06-27 13:32:59 -0500

    Finale

--- src/app/services/diagrams.service.ts
+++ src/app/services/diagrams.service.ts
@@ -7,7 +7,7 @@ import { Observable } from 'rxjs';
 })
 export class DiagramsService {
 
-  private apiUrl = 'https://du8u287sjk.execute-api.us-east-1.amazonaws.com/diagram';
+  private apiUrl = 'https://wadl1xe2lh.execute-api.us-east-1.amazonaws.com/dev/diagram';
 
   constructor(private http: HttpClient) { }
 
